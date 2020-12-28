62e15e9b770ee20d790faf4a98814b67466a62f0
Lewos <digol1023@gmail.com>
2020-12-28 13:34:23 -0300

    Version: 0.0.0.3: - Fixed extraction due to website template change. - Code refactoring.

--- IMABI.Extractor/Program.cs
+++ IMABI.Extractor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AngleSharp;
 using AngleSharp.Dom;
+using IMABI.Extractor.Utils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,30 +13,12 @@ namespace IMABI.Extractor
 {
     class Program
     {
-        static string ReturnPage(string imabiWebsite)
-        {
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(imabiWebsite);
-            httpWebRequest.UserAgent = "PostmanRuntime/7.26.1";
-
-            string page = default(string);
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (Stream dataStream = httpWebResponse.GetResponseStream())
-            {
-                StreamReader streamReader = new StreamReader(dataStream);
-                page = streamReader.ReadToEnd();
-            }
-
-            return page;
-        }
-
         static async Task Main(string[] args)
         {
             Console.WriteLine("IMABI Extractor");
-            Console.WriteLine("Transform website version into html/pdf to study offline.");
+            Console.WriteLine("Transform website version into html to study offline. You can \"print\" to PDF too. ");
 
             string imabiWebsite = "https://www.imabi.net/tableofcontents.htm";
-            string htmlCode = default(string);
             
             IConfiguration config = Configuration.Default;
             IBrowsingContext context = BrowsingContext.New(config);
@@ -45,48 +28,19 @@ namespace IMABI.Extractor
                 string currentDir = currentDir = string.Format("{0}\\{1}", Directory.GetCurrentDirectory(), "IMABIWebsiteLessons.html");
                 Console.WriteLine("File will be saved on: " + currentDir);
 
+                string htmlCode = IMABIUtils.GetPageContent(imabiWebsite);
+                IDocument document = await context.OpenAsync(req => req.Content(htmlCode));
+                IList<string> allUniqueLessonsLinkList = IMABIUtils.GetURLLessons(document);
+
+                Console.WriteLine("Extracting lessons...");
                 using (StreamWriter file = new StreamWriter(currentDir))
                 {
-                    // Website request.                    
-                    htmlCode = ReturnPage(imabiWebsite);
-                    IDocument document = await context.OpenAsync(req => req.Content(htmlCode));
-
-                    // Getting all lessons links from the 'https://www.imabi.net/tableofcontents.htm'
-                    IEnumerable<string> allLessonsLinks = document.QuerySelectorAll("a.fw_link_page").Attr("href");
-
-                    // Removing all repeated links.
-                    IList<string> allLessonsUniqueLinks = new List<string>();
-                    foreach (string s in allLessonsLinks)
+                    foreach (string lessonLink in allUniqueLessonsLinkList)
                     {
-                        if (!allLessonsUniqueLinks.Any(x => x.Contains(s)))
-                        {
-                            allLessonsUniqueLinks.Add(s);
-                        }
-                        else
-                        {
-                            Console.WriteLine(string.Format("Repeated: {0}", s));
-                        }
-                    }
-
-                    // Write table of contents to file.
-                    Console.WriteLine("Tables of Content.");
-                    IElement eph = document.QuerySelectorAll("div").GetElementById("fw-bigcontain");
-                    file.WriteLine(eph.OuterHtml);
-
-                    // Write all lessons to file.
-                    Console.WriteLine("All Lessons");
-                    foreach (string lessonLink in allLessonsUniqueLinks)
-                    {
+                        Console.WriteLine(lessonLink);
                         try
                         {
-                            // Content.
-                            Console.WriteLine(lessonLink);
-                            htmlCode = ReturnPage(lessonLink);
-
-                            document = await context.OpenAsync(req => req.Content(htmlCode));
-                            eph = document.QuerySelectorAll("div").GetElementById("fw-bigcontain");
-
-                            file.WriteLine(eph.OuterHtml);
+                            file.WriteLine(IMABIUtils.GetLessonContent(lessonLink, context));
                         }
                         catch (Exception e)
                         {
@@ -94,7 +48,6 @@ namespace IMABI.Extractor
                             Console.WriteLine(e.StackTrace);
                         }
                     }
-
                 }
 
             }
@@ -104,7 +57,7 @@ namespace IMABI.Extractor
                 Console.WriteLine(e.StackTrace);
             }
 
-            Console.WriteLine("Press any button to exit...");
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();            
         }
     }
